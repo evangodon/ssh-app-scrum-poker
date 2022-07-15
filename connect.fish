a923c728b6adee7f4612d780e8f835dcd0c620be
Evan Godon <evan.godon@vts.com>
2022-07-15 11:56:54 -0400

    update ssh connect script

--- connect.fish
+++
@@ -1,7 +0,0 @@
-
-while true
-  sleep 5
-  clear
-  ssh evan.godon@127.0.0.1 -p 44444
-  commandline -f repaint
-end
---
+++ dev-ssh-connect.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+while true
+do
+  clear
+  echo "Connecting..."
+  ssh 127.0.0.1 -p 44444
+  sleep 3
+done
+
+
